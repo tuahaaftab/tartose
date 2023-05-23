89fd961cb71d88492bebe4cc0add95226d49a136
Replit user <>
2023-05-23 00:11:34 +0000

    Adds basic list

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import re
 import json
+from datetime import datetime
 
-# transformed_json
-output_json_object = {}
+# filepath = "./inputs/sample_input.json"
+filepath = "./inputs/custom_input.json"
 
-filepath = "./inputs/sample_input.json"
+def has_rfc3339_format(string):
+  pattern = r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z"
+  match = re.search(pattern, string)
+
+  if match:
+    return True
+  return False
+
+
+def rfc3339_to_epoch(rfc3339_string):
+  timestamp_format = "%Y-%m-%dT%H:%M:%SZ"
+  dt = datetime.strptime(rfc3339_string, timestamp_format)
+  return int(dt.timestamp())
 
 
 def print_json_object(json_object):
@@ -18,33 +32,82 @@ def read_input(filepath):
     return data
 
 
-def cast_to_string(s):
+# Process String
+def process_string(s):
   valid = True
-  s = s.strip()
-  
+
   if s == "":
     valid = False
-  
+  elif has_rfc3339_format(s):
+    s = rfc3339_to_epoch(s)
+
   return (valid, s)
 
 
-def cast_to_number(s):
-  return float(s.strip())
+# Process Number
+def process_number(s):
+  if s == "":
+    return (False, s)
+
+  num = 0
+  try:
+    num = int(s)
+    return (True, num)
+  except ValueError:
+    num = 0
+
+  try:
+    num = float(s)
+    return (True, num)
+  except ValueError:
+    num = 0
+
+  return (False, num)
 
 
-def cast_to_bool(s):
-  true_values = ["1", "t", "T", "TRUE", "true", "True"]
-  false_values = ["0", "f", "F", "FALSE", "false", "False"]
+true_values = ["1", "t", "T", "TRUE", "true", "True"]
+false_values = ["0", "f", "F", "FALSE", "false", "False"]
+
+
+# Process Boolean
+def process_bool(s):
 
   s = s.strip()
 
   if s in true_values:
-    return True
+    return (True, True)
   elif s in false_values:
-    return False
+    return (True, False)
+  else:
+    return (False, None)
+
+
+# Process Null
+def process_null(s):
+  if s in true_values:
+    return (True, None)
+  else:
+    return (False, s)
+
+
+# Process List
+def process_list(l):
+  # Empty List | "L": "noop" | Expecting square brackets "L": ["noop"]
+  if not isinstance(l, list):
+    return (False, l)
+
+  # for elem in l:
+  #   if isinstance(elem, dict):
+  #     print(elem)
+
+  return (True, l)
 
 
 # Main
+
+# transformed json object
+output_json_object = {}
+
 input_json_object = read_input(filepath)
 # print_json_object(input_json_object)
 
@@ -59,15 +122,18 @@ for key, data in input_json_object.items():
   output_value = data[data_type]
 
   valid = True
-  
+
   # Based on value_type, cast to appropriate value
   if data_type == "N":
-    casted_output_value = cast_to_number(output_value)
+    valid, casted_output_value = process_number(output_value.strip())
   elif data_type == "BOOL":
-    casted_output_value = cast_to_bool(output_value)
+    valid, casted_output_value = process_bool(output_value.strip())
   elif data_type == "S":
-    valid, casted_output_value = cast_to_string(output_value)
-      
+    valid, casted_output_value = process_string(output_value.strip())
+  elif data_type == "NULL":
+    valid, casted_output_value = process_null(output_value.strip())
+  elif data_type == "L":
+    valid, casted_output_value = process_list(output_value)
 
   if valid:
     output_json_object[key] = casted_output_value
